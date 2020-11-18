2102bf090b1a89e49ce601f42f599eb5f15252b4
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-11-18 14:14:24 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.West)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.East)
 })
